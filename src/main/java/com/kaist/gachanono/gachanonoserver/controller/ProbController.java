1f93832a92eb0e82db10f22838b80d33f352a797
HYUNGKIM <rlagud1123@gmail.com>
2022-04-12 23:43:35 +0900

    add mock-up;

---
+++ src/main/java/com/kaist/gachanono/gachanonoserver/controller/ProbController.java
@@ -0,0 +1,55 @@
+package com.kaist.gachanono.gachanonoserver.controller;
+
+import javax.servlet.http.HttpSession;
+
+import com.kaist.gachanono.gachanonoserver.config.auth.SessionUser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller 
+@RequestMapping("/prob")
+public class ProbController {
+    
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private HttpSession httpSession;
+
+    @GetMapping("/calculation") 
+    public String calculation(Model model) {
+        // Vue 예제 페이지로 이동
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        
+        logger.info("user[{}]", user.toString());
+
+        return "/prob/calculation"; 
+    } 
+
+    @GetMapping("/report") 
+    public String report(Model model) {
+        // Vue 예제 페이지로 이동
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        
+        logger.info("user[{}]", user.toString());
+
+        return "/prob/report"; 
+    } 
+
+    @GetMapping("/simulation")
+    public String simulation(Model model) {
+        // Vue 예제 페이지로 이동
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        
+        logger.info("user[{}]", user.toString());
+
+        return "/prob/simulation";
+    } 
+
+}
--- src/main/resources/static/assets/demo/chart-bar-demo.js
+++ src/main/resources/static/assets/demo/chart-bar-demo.js
@@ -7,20 +7,17 @@ var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["0번 뽑힐 확률", "1번 뽑힐 확률", "2번 뽑힐 확률"],
     datasets: [{
       label: "Revenue",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [0.81, 0.18, 0.01],
     }],
   },
   options: {
     scales: {
       xAxes: [{
-        time: {
-          unit: 'month'
-        },
         gridLines: {
           display: false
         },
@@ -31,7 +28,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 1,
           maxTicksLimit: 5
         },
         gridLines: {
