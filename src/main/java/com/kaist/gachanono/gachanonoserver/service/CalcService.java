8a81646949ec2d82ce0ce2429f235328a75148fc
HYUNGKIM <rlagud1123@gmail.com>
2022-05-01 21:56:10 +0900

    Add CalcService and ChartDataService;

---
+++ src/main/java/com/kaist/gachanono/gachanonoserver/service/CalcService.java
@@ -0,0 +1,11 @@
+package com.kaist.gachanono.gachanonoserver.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CalcService {
+    
+}
