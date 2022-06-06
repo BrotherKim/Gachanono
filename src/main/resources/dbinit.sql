51d9164ba22a8c0d04ca4509afe9e7ac3a9478c3
HYUNGKIM <rlagud1123@gmail.com>
2022-06-06 09:36:24 +0900

    DB dumping;

--- src/main/resources/dbinit.sql
+++ src/main/resources/dbinit.sql
@@ -1,61 +1,217 @@
-CREATE TABLE `gachanono`.`posts` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(255) NOT NULL,
-  `content` TEXT NOT NULL,
-  `writer` VARCHAR(255) NOT NULL,
-  `view` BIGINT NOT NULL,
-  `created_date` VARCHAR(255) NOT NULL,
-  `modified_date` VARCHAR(255) NOT NULL,
-  `user_id` BIGINT NOT NULL,
-  `good` BIGINT NOT NULL,
-  `game_id` VARCHAR(255) NOT NULL,
-  `gamename` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`));
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gachanono
+-- ------------------------------------------------------
+-- Server version	8.0.28
 
-CREATE TABLE `gachanono`.`user` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255),
-  `nickname` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `created_date` VARCHAR(255) NOT NULL,
-  `modified_date` VARCHAR(255) NOT NULL,
-  `role` VARCHAR(255) NOT NULL,
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_date` varchar(255) NOT NULL,
+  `modified_date` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
-  UNIQUE INDEX `nickname_UNIQUE` (`nickname` ASC) VISIBLE,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `nickname_UNIQUE` (`nickname`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE posts add FOREIGN KEY(user_id) REFERENCES user(id);
 
-CREATE TABLE `gachanono`.`comments` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `comment` TEXT NOT NULL,
-  `created_date` VARCHAR(255) NOT NULL,
-  `modified_date` VARCHAR(255) NOT NULL,
-  `posts_id` BIGINT NOT NULL,
-  `user_id` BIGINT NOT NULL,
-  PRIMARY KEY (`id`));
+--
+-- Table structure for table `game`
+--
 
-ALTER TABLE comments add FOREIGN KEY(posts_id) REFERENCES posts(id);
-ALTER TABLE comments add FOREIGN KEY(user_id) REFERENCES user(id);
+DROP TABLE IF EXISTS `game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `game` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `gamename` varchar(255) NOT NULL,
+  `probfilename` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `gamename_UNIQUE` (`gamename`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `gachanono`.`game` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `gamename` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`));
-ALTER TABLE posts add FOREIGN KEY(game_id) REFERENCES game(id);
 
-CREATE TABLE `gachanono`.`gacha` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `gachaname` VARCHAR(255) NOT NULL,
-  `templatefilename` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`));
+--
+-- Table structure for table `posts`
+--
 
-CREATE TABLE `gachanono`.`item` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `itemname` VARCHAR(255) NOT NULL,
-  `game_id` BIGINT NOT NULL,
-  PRIMARY KEY (`id`));
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `writer` varchar(255) NOT NULL,
+  `view` bigint NOT NULL,
+  `created_date` varchar(255) NOT NULL,
+  `modified_date` varchar(255) NOT NULL,
+  `user_id` bigint NOT NULL,
+  `good` bigint NOT NULL,
+  `game_id` bigint NOT NULL,
+  `gamename` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `game_id` (`game_id`),
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE item add FOREIGN KEY(game_id) REFERENCES game(id);
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `itemname` varchar(255) NOT NULL,
+  `gameid` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `itemname_UNIQUE` (`itemname`),
+  KEY `item_ibfk_1_idx` (`gameid`),
+  CONSTRAINT `item_ibfk_1` FOREIGN KEY (`gameid`) REFERENCES `game` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `gacha`
+--
+
+DROP TABLE IF EXISTS `gacha`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gacha` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `gachaname` varchar(255) NOT NULL,
+  `templatefilename` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `gachaname_UNIQUE` (`gachaname`),
+  UNIQUE KEY `templatefilename_UNIQUE` (`templatefilename`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `comment` text NOT NULL,
+  `created_date` varchar(255) NOT NULL,
+  `modified_date` varchar(255) NOT NULL,
+  `posts_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `posts_id` (`posts_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`posts_id`) REFERENCES `posts` (`id`),
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (2,'sdfgsfdgsfdㅁㄴㅇㄹ','2022.05.31 16:44','2022.05.31 16:44',4,1),(8,'ㅁㄴㅇㄻㄴㅇㄹ','2022.06.05 21:39','2022.06.05 21:39',4,1),(9,'ㅁㄴㅇㄹ','2022.06.05 21:40','2022.06.05 21:40',4,1),(11,'asdf','2022.06.06 08:38','2022.06.06 08:38',8,1);
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- Dumping data for table `gacha`
+--
+
+LOCK TABLES `gacha` WRITE;
+/*!40000 ALTER TABLE `gacha` DISABLE KEYS */;
+INSERT INTO `gacha` VALUES (1,'컴플리트가챠','completegacha.csv'),(2,'다른 아이템 구간별 확률','seg_diff.csv'),(3,'동일 아이템 구간별 확률','seg_same.csv'),(4,'천장이 있는 복원추출','swr_ceiling.csv'),(5,'복원추출','swr.csv');
+/*!40000 ALTER TABLE `gacha` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `game`
+--
+
+LOCK TABLES `game` WRITE;
+/*!40000 ALTER TABLE `game` DISABLE KEYS */;
+INSERT INTO `game` VALUES (1,'카트라이더러쉬플러스','kartriderrushplus.csv'),(2,'메이플스토리','maplestory.csv');
+/*!40000 ALTER TABLE `game` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,'아이템1',1),(2,'아이템123',2),(3,'아이템980',1);
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (2,'sdfgfhgjdfhgjfhg','fhgjfhgjfghjf','김형',21,'2022.05.24','2022.05.24',1,0,1,'카트라이더러쉬플러스'),(3,'asdfasdfasd','asdf','김형',5,'2022.05.29','2022.05.29',1,2,1,'카트라이더러쉬플러스'),(4,'asdf','asdf','김형',27,'2022.05.31','2022.05.31',1,7,1,'카트라이더러쉬플러스'),(5,'메이플스토리asdf','asdf','김형',43,'2022.06.02','2022.06.05 22:19',1,1,2,'메이플스토리'),(8,'adsf','asdf','김형',2,'2022.06.06','2022.06.06',1,0,1,'카트라이더러쉬플러스');
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'rlagud1123@gmail.com',NULL,'김형','rlagud1123@gmail.com','2022.05.24','2022.06.06 09:25','SOCIAL');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-06  9:31:10
